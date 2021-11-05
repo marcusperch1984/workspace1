1261807379ee0f45266e3624bed4ce858263f1f0
Marcus Perch <marcus.perch@ocado.com>
2021-11-05 17:02:34 +0000

    add python example 2

---
+++ python_example_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#import pandas as pd
+
+print("hello world")
+
+# Let's try and save something in an array (think of it as a table)
+
+#df1 = pd.DataFrame({'Name': ['Pankaj', 'Lisa'], 'ID': [1, 2]})
+#df2 = pd.DataFrame({'Name': ['David'], 'ID': [3]})
+#df3 = pd.DataFrame({'Name': ['Marcus'], 'ID': [4]})
+#df4 = pd.DataFrame({'Name': ['Filip'], 'ID': [5]})
+
+#print("********************** df1 = ")
+#print(df1)
+
+
+#print("********************** df2 = ")
+#print(df2)
+
+#df5 = df1.append(df2).append(df3).append(df4)
+#df5.reset_index(drop=True, inplace=True)
+
+#print('\nResult DataFrame:\n', df5)
+
+# print(my_table)
+
+# my_table.append("col1" : "a", "col2" : "b")
+
+# print(my_table)
