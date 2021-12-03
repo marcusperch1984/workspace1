b1b67ab93a800e3007f6d25a65ff8060c6dccb64
Marcus Perch <marcus.perch@ocado.com>
2021-12-03 16:49:54 +0000

    add if/ese

--- python_example_2.py
+++ python_example_2.py
@@ -2,32 +2,28 @@
 
 #import pandas as pd
 
-a = [100, 400, 300]
+a = [100, 400] #, 300]
+
+p1 = a[0]
+p2 = a[1]
+#p3 = a[2]
 
 print("hello world - array")
-print(a[0])
 
-# Let's try and save something in an array (think of it as a table)
-
-#df1 = pd.DataFrame({'Name': ['Pankaj', 'Lisa'], 'ID': [1, 2]})
-#df2 = pd.DataFrame({'Name': ['David'], 'ID': [3]})
-#df3 = pd.DataFrame({'Name': ['Marcus'], 'ID': [4]})
-#df4 = pd.DataFrame({'Name': ['Filip'], 'ID': [5]})
-
-#print("********************** df1 = ")
-#print(df1)
+# 1) if / else
 
 
-#print("********************** df2 = ")
-#print(df2)
+if p1 > p2:
+	print("p1 is the winner with " + str(p1))
+else:
+	print("p2 is the winner with " + str(p2))
 
-#df5 = df1.append(df2).append(df3).append(df4)
-#df5.reset_index(drop=True, inplace=True)
 
-#print('\nResult DataFrame:\n', df5)
 
-# print(my_table)
+# 2) loop
 
-# my_table.append("col1" : "a", "col2" : "b")
 
-# print(my_table)
+
+#print"stra[0]"
+
+
